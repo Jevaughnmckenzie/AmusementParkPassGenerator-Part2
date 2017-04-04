bedfedb4c62959e39eed8d6fa0a4de109d2b095f
Jevaughn Mckenzie <jmckenzie1004@gmail.com>
2017-04-04 18:41:12 -0400

    Storyboard UI is complete.

---
+++ SingleViewAppSwiftTemplate/PassController.swift
@@ -0,0 +1,29 @@
+//
+//  PassController.swift
+//  AmusementParkPassGenerator
+//
+//  Created by Jevaughn McKenzie on 4/4/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class PassController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
